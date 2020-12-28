347225a04813f69a9deeced4ce2f023b3ac88b5d
Shatha96 <shathabast62@gmail.com>
2020-12-28 11:40:45 +0200

    First commit

---
+++ Shatha.java
@@ -0,0 +1,8 @@
+public class Shatha {
+    public static void main(String[] args) {
+        System.out.println("My name is Shatha");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Nablus, Palestine");
+    }
+}
+
